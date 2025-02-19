60134d580871dac081d551ac701cae784d5e946e
EikrozGraphics <sauleikroz@gmail.com>
2025-02-19 02:46:49 -0600

    config.js

--- config.js
+++ config.js
@@ -98,19 +98,24 @@ popcat : 'https://api.popcat.xyz',
 xcoders : 'https://api-xcoders.site'
 },
    
+exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
+   
 global.APIKeys = { 
-'https://api.xteam.xyz': `${keysxteam}`,
-'https://api.lolhuman.xyz': `${lolkeysapi}`,
-'https://api.neoxr.my.id': `${keysneoxr}`,	
-'https://violetics.pw': 'beta',
+'https://api.xteam.xyz': `${keysxteam}`, -no
+'https://api.lolhuman.xyz': `${78ad673c6e077129afb7443a}`,
+'https://api.neoxr.my.id': `${SAuj6T}`,	
+'https://violetics.pw': 'beta', -no
 'https://api.zahwazein.xyz': `${keysxxx}`,
-'https://api.fgmods.xyz': 'DRLg5kY7', 
-'https://api-fgmods.ddns.net': 'fg-dylux',
-'https://api.botcahx.biz.id': 'Admin',
-'https://api.ibeng.tech/docs': 'tamvan',
-'https://api.itsrose.site': 'Rs-Zeltoria',
-'https://api-xcoders.site': 'Frieren'
+'https://api.fgmods.xyz': '5LAai552', 
+'https://api-fgmods.ddns.net': 'fg-dylux', -no
+'https://api.botcahx.biz.id': 'Admin', -no
+'https://api.ibeng.tech/docs': 'tamvan', -no
+'https://api.itsrose.site': 'Rs-Zeltoria', -no
+'https://api-xcoders.site': 'Frieren' -no
 }
+
+exports.SPIDER_API_TOKEN = "QIjBrDig8SV2DG1AJKbr";
+
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
